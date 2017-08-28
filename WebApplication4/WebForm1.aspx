ea37caeff88268cc7e931cab89918d18cd92da81
igorapp <igorgrzesista@gmail.com>
2017-08-28 03:26:49 +0200

    done

---
+++ WebApplication4/WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication4.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Guess a number (1-100)</div>
+        <p>
+            <asp:TextBox ID="text" runat="server" OnTextChanged="text_TextChanged" style="height: 22px"></asp:TextBox>
+            <asp:Button ID="checkButton" runat="server" Text="Check!" Height="26px" OnClick="checkButton_Click" />
+        </p>
+        <p>
+            <asp:Label ID="serverAnswerLabel" runat="server" Text=""></asp:Label>
+        </p>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ WebApplication4/WebForm1.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ViewState["Number"] = new Random().Next(1, 101);
+            }
+        }
+
+        protected void checkButton_Click(object sender, EventArgs e)
+        {
+
+            int guess = Convert.ToInt32(text.Text);
+            int number = (int)ViewState["Number"];
+
+            if (guess > number)
+            {
+                this.serverAnswerLabel.Text = "Look for lower number";
+            }
+            if (guess < number)
+            {
+                this.serverAnswerLabel.Text = "Look for higher number";
+            }
+            if (guess == number)
+            {
+                this.serverAnswerLabel.Text = "Right!";
+            }
+
+        }
+
+        protected void text_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
